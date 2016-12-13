55ef1c1679202ed68ba414c9e14c8b8211791564
Brad Duthie <brad.duthie@gmail.com>
2016-12-13 15:55:02 +0000

    Add in some code to read in the RESOURCE_1 matrix

--- src/resource.c
+++ src/resource.c
@@ -6,43 +6,37 @@
 /* Just add a number by two to test */
 /* TODO: Arguments: RESOURCE_1, RESOURCE_2, VECTOR_OF_PARAMETER_VALUES */
 SEXP resource(SEXP RESOURCE_1){
+ 
+    /* SOME STANDARD DECLARATIONS OF KEY VARIABLES AND POINTERS               */
+    /* ====================================================================== */
+    int protected_n;
+    int *dimX;
+    double *R_ptr_1;
+    double *ansptr;
 
     /* First take care of all the reading in of code from R to C */
     /* ====================================================================== */
-    
-    /* Below tells R not to delete the object if garbage collector activated */
-    PROTECT(RESOURCE_1 = AS_INTEGER(RESOURCE_1));
 
-    /* Defines and protects a new array to return */
-    SEXP RESOURCE_1_NEW;
-    PROTECT(RESOURCE_1_NEW = NEW_INTEGER(1));
+    protected_n = 0;
+    
+    PROTECT(RESOURCE_1 = AS_NUMERIC(RESOURCE_1));
+    protected_n++;
+    
+    R_ptr_1 = REAL(RESOURCE_1);
+    
+    dimX = INTEGER(GET_DIM(RESOURCE_1));
+    
+    
+    SEXP ans;
+    PROTECT(ans = allocMatrix(REALSXP, 1, 2));
+    protected_n++;
+    
+    ansptr = REAL(ans);
+    ansptr[0] = dimX[0];
+    ansptr[1] = dimX[1];
+    
+    UNPROTECT(protected_n);
+    
 
-    /* PROTECT(ans = allocMatrix(RESOURCE_1_NEW, nx, ny)); */
-    
-    /* New structure below to check the dimensions of each resource */
-    SEXP dims;
-    PROTECT(dims = NEW_INTEGER(1));
-    
-    
-    /* Some more familiar c code down here */
-    /* ====================================================================== */
-        double resource_1, resource_n, stocha;
-    
-    stocha     = runif(0, 1);
-    resource_1 = asReal(RESOURCE_1);
-    resource_n = resource_1 + (100 * stocha);
-    resource_n = floor(resource_n);
-
-    /*-------------------------------------*/
-    
-    dims = getAttrib(RESOURCE_1, install("dim"));
-    
-    /* INTEGER(RESOURCE_1_NEW)[0] = resource_n; */
-        
-    UNPROTECT(3);
-
-    return dims; /* RESOURCE_1_NEW; */
+    return(ans); /* RESOURCE_1_NEW; */
 }
-
-
-/* R_xlen_t n = xlength(x) */
