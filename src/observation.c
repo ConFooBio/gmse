c1a9faec27df93cdc86f640ec2fd61659da42cb2
Brad Duthie <brad.duthie@gmail.com>
2017-01-13 10:41:47 +0000

    Add the resource moving function into the observation c file

--- src/observation.c
+++ src/observation.c
@@ -4,6 +4,165 @@
 #include <Rmath.h>
 
 /* =============================================================================
+ * This function moves individuals on the landscape according to some rules.
+ * It is identical to the same function in resource.c, but I'm including it here
+ * in the interest of modularity. Eventually, these types of functions might
+ * be better moved into their own file, which each higher-level G-MSE function
+ * can call.
+ * The 'edge' argument defines what happens at the landscape edge:
+ *     0: Nothing happens (individual is just off the map)
+ *     1: Torus landscape (individual wraps around to the other side)
+ * The 'type' argument defines the type of movement allowed:
+ *     0: No movement is allowed
+ *     1: Movement is random uniform from zero to move_para in any direction
+ *     2: Movement length is poisson(move_para) in x then y direction
+ *     3: Movement length is poisson(move_para) in any direction
+ * ========================================================================== */
+void res_mover(double **res_moving, int xloc, int yloc, int move_para, int rows,
+           int edge, double **landscape, int land_x, int land_y, int type){
+    
+    int res_num;      /* Resource number index                       */
+    int move_len;     /* Length of a move                            */
+    int move_dir;     /* Move direction (-1 or 1)                    */
+    int new_pos;      /* New position: check if over landscape edge  */
+    double rand_num;  /* Random number used for sampling             */
+    double rand_uni;  /* Random uniform number                       */
+    double rand_pois; /* Random poisson number                       */
+    double raw_move;  /* Movement length before floor() truncation   */
+
+    for(res_num=0; res_num < rows; res_num++){
+        /* Move first in the xloc direction --------------------------------- */
+        new_pos  = res_moving[res_num][xloc];
+        rand_num = 0.5;
+        do{ /* Note that rand_num can never be exactly 0.5 */
+            rand_num = runif(0, 1);
+        } while(rand_num == 0.5);
+        if(rand_num > 0.5){
+            move_dir = 1;   
+        }else{
+            move_dir = -1;   
+        } /* Now we have the direction the resource is moving */
+        switch(type){
+            case 0: /* No change in position */
+                move_len = 0;
+                break;
+            case 1: /* Uniform selection of position change */
+                do{ /* Again, so that res_num never moves too far */
+                    rand_uni = runif(0, 1);
+                } while(rand_uni == 1.0);
+                raw_move = rand_uni * (res_moving[res_num][move_para] + 1);
+                move_len = (int) floor(raw_move);
+                break;
+            case 2: /* Poisson selection of position change */
+                rand_pois = rpois(res_moving[res_num][move_para]);    
+                raw_move  = rand_pois * (res_moving[res_num][move_para] + 1);
+                move_len  = (int) floor(raw_move);
+                break;
+            case 3: /* Uniform position movement a Poisson number of times */
+                rand_pois = rpois(res_moving[res_num][move_para]);
+                raw_move  = 0;
+                while(rand_pois > 0){
+                    do{
+                        rand_uni = runif(0, 1);
+                    } while(rand_uni == 1.0);
+                    raw_move += rand_uni * (res_moving[res_num][move_para] + 1);
+                    rand_pois--;
+                }
+                move_len = (int) floor(raw_move);
+                break;
+            default:
+                if(res_num == 0){
+                    printf("Unclear specification of movement type \n");
+                }
+                break;
+        }
+        new_pos  = res_moving[res_num][xloc] + (move_dir * move_len); 
+        if(new_pos > land_x || new_pos < 0){ /* If off the edge */
+            switch(edge){
+                case 0: /* Nothing happens (effectively, no edge) */
+                    break;
+                case 1: /* Corresponds to a torus landscape */
+                    while(new_pos > land_x){
+                        new_pos = new_pos - land_x; 
+                    }
+                    while(new_pos < 0){
+                        new_pos = new_pos + land_x;
+                    }
+                    break;
+                default:
+                    if(res_num == 0){
+                        printf("ERROR: Edge effects set incorrectly \n");
+                    }
+                    break;
+            }
+        }
+        res_moving[res_num][xloc] = new_pos;
+        /* Move next in the yloc direction ---------------------------------- */
+        new_pos  = res_moving[res_num][yloc];
+        rand_num = 0.5;
+        do{ /* Note that rand_num can never be exactly 0.5 */
+            rand_num = runif(0, 1);
+        } while(rand_num == 0.5);
+        if(rand_num > 0.5){
+            move_dir = 1;   
+        }else{
+            move_dir = -1;   
+        } /* Now we have the direction the resource is moving */
+        switch(type){
+            case 0: /* No change in position */
+                move_len = 0;
+                break;
+            case 1: /* Uniform selection of position change */
+                do{ /* Again, so that res_num never moves too far */
+                    rand_uni = runif(0, 1);
+                } while(rand_uni == 1.0);
+                raw_move = rand_uni * (res_moving[res_num][move_para] + 1);
+                move_len = (int) floor(raw_move);
+                break;
+            case 2: /* Poisson selection of position change */
+                rand_pois = rpois(res_moving[res_num][move_para]);    
+                raw_move  = rand_pois * (res_moving[res_num][move_para] + 1);
+                move_len  = (int) floor(raw_move);
+                break;
+            case 3: /* Uniform position movement a Poisson number of times */
+                rand_pois = rpois(res_moving[res_num][move_para]);
+                raw_move  = 0;
+                while(rand_pois > 0){
+                    do{
+                        rand_uni = runif(0, 1);
+                    } while(rand_uni == 1.0);
+                    raw_move += rand_uni * (res_moving[res_num][move_para] + 1);
+                    rand_pois--;
+                }
+                move_len = (int) floor(raw_move);
+                break;
+            default:
+                break;
+        }
+        new_pos  = res_moving[res_num][yloc] + (move_dir * move_len); 
+        if(new_pos > land_y || new_pos < 0){ /* If off the edge */
+            switch(edge){
+                case 0: /* Nothing happens (effectively, no edge) */
+                    break;
+                case 1: /* Corresponds to a torus landscape */
+                    while(new_pos > land_y){
+                        new_pos = new_pos - land_y;  
+                    }
+                    while(new_pos < 0){
+                        new_pos = new_pos + land_y;   
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        res_moving[res_num][yloc] = new_pos;
+    }
+}
+/* ===========================================================================*/
+
+
+/* =============================================================================
  * This function moves one on the landscape according to some rules
  * For now, it is repeated in both c files, but if there are more functions
  * that serve two purposes, then a general utilily c file might be created
@@ -560,7 +719,7 @@ SEXP observation(SEXP RESOURCE, SEXP LANDSCAPE, SEXP PARAMETERS, SEXP AGENT){
 
     /* This switch function calls a method of population size estimation */
     switch(method){
-       case 0: /* Start of observation column, the +1 skips agent type col */
+       case 0: /* Start of obs column, the +1 skips a_type col added later */
            obs_iter = trait_number + 1; 
            while(times_obs > 0){
                mark_res(resource_array, agent_array, land, paras, res_number, 
@@ -568,6 +727,9 @@ SEXP observation(SEXP RESOURCE, SEXP LANDSCAPE, SEXP PARAMETERS, SEXP AGENT){
                times_obs--; /* Then move agents if need be for new sample */
                obs_iter++;
            }
+           break;
+       case 1:
+           
            break;
        default:
            printf("ERROR: No observation method set: marking all in view \n");
