d2c88ca14cb36bab53569735ebbc844d6274f8d6
Brad Duthie <brad.duthie@gmail.com>
2017-03-03 15:45:12 +0000

    Make cell values on layer 2 random uniform

--- R/landscape.R
+++ R/landscape.R
@@ -7,9 +7,12 @@
 #'@param cols The dimension of one side of the landscape (e.g., Latitude)
 #'@param rows The dimension of the other side of the landscape (e.g., Longitude)
 #'@param cell_types Scalar or vector of all possible types of landscape cells
+#'@param cell_val_mn Mean cell value (e.g., defining crop output on a cell)
+#'@param cell_val_sd Standard devation of cell values on a landscape
 #'@return the_land A cols by rows landscape with randomly distributed cell types
 #'@export
-make_landscape <- function(model, rows, cols, cell_types){
+make_landscape <- function(model, rows, cols, cell_types, cell_val_mn,
+                           cell_val_sd){
     the_land  <- NULL;
     if(model == "IBM"){
         if(rows < 2){
@@ -21,8 +24,8 @@ make_landscape <- function(model, rows, cols, cell_types){
         cell_count     <- cols * rows;
         the_terrain    <- sample(x = cell_types, size = cell_count, 
                                  replace = TRUE);
-        the_terrain2   <- sample(x = cell_types, size = cell_count, 
-                                 replace = TRUE);
+        the_terrain2   <- rnorm(n = cell_count, mean = cell_val_mn,
+                                sd = cell_val_sd);
         alldata        <- c(the_terrain, the_terrain2);
         the_land       <- array(data = alldata, dim = c(rows, cols, 2));
     }
