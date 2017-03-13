eea1aa70c8d483ea8b0a104aec247d4f792d709f
Brad Duthie <brad.duthie@gmail.com>
2017-03-13 10:34:19 +0000

    Initialise game file

---
+++ src/game.c
@@ -0,0 +1,9 @@
+#include "utilities.h"
+
+/* =============================================================================
+ * This file is a work in progress, which will have all of the necessary
+ * functions for running the genetic algorithm. Separate functions will 
+ * call the genetic algorithm from R and C (C is default in G-MSE), and this
+ * file will link with the user.c file to run a genetic algorithm for each
+ * unique individual agent.
+ * ========================================================================== */
