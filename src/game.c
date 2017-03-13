af21a1ec9482a12d8bff4539d09c1784b12430da
Brad Duthie <brad.duthie@gmail.com>
2017-03-13 17:42:22 +0000

    Some more edits to the initialisation of the array, but a lot will need to change now (see notes)

--- src/game.c
+++ src/game.c
@@ -14,6 +14,10 @@
  *     UTILITY: A full 3D utility array
  *     layer: The layer on which the minimum is going to be found
  *     budget: The total budget that the agent has to work with (initliases)
+ *     x0: Row to start randomising values
+ *     x1: Row to stop randomising values
+ *     y0: Column to start randomising values
+ *     y1: Column to stop randomising values
  * ========================================================================== */
 int min_cost(double ***UTILITY, int layer, double budget, int x0, int x1,
              int y0, int y1){
@@ -39,7 +43,42 @@ int min_cost(double ***UTILITY, int layer, double budget, int x0, int x1,
  * Necessary variable inputs include:
  *     UTILITY: A 3D array of utility values
  *     layer: The 'z' layer of the UTILITY array to be initialised
- *     pop_size: The size of the total population
+ *     pop_size: The size of the total population (layers to population)
  *     carbon_copies: The number of identical agents used as seeds
  *     budget: The budget that random agents have to work with
+ *     UTILITY_rows: Number of rows in the UTILITY array
+ *     UTILITY_cols: Number of columns in the UTILITY array 
+ *     population: array of the population that is made (malloc needed earlier)
+ *     x0: Row to start randomising values
+ *     x1: Row to stop randomising values
+ *     y0: Column to start randomising values
+ *     y1: Column to stop randomising values
  * ========================================================================== */
+void initialise_pop(double ***UTILITY, int layer, int pop_size, int budget,
+                    int carbon_copies, int UTILITY_rows, int UTILITY_cols,
+                    double ***population, int x0, int x1, int y0, int y1){
+    
+    int agent;
+    int row, col;
+    double lowest_cost;
+
+    /* First read in pop_size copies of the UTILITY layer of interest */
+    for(agent = 0; agent < pop_size; agent++){
+        for(row = 0; row < UTILITY_rows; row++){
+            for(col = 0; col < UTILITY_cols; col++){
+                population[row][col][agent] = UTILITY[row][col][layer];
+            }
+        }
+        /* Re-assign values where it is necessary */
+        if(agent >= carbon_copies){
+            for(row = x0; row < x1; row++){
+                for(col = y0; col < y1; col++){
+                    population[row][col][agent] = 0;
+                }
+            }
+            lowest_cost =  min_cost(UTILITY, layer
+        }
+    }
+    
+    
+}
