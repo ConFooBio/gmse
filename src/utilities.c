a5f3e441092e38f798fcdbe35b8d1041cae023b7
Brad Duthie <brad.duthie@gmail.com>
2017-05-26 10:11:18 +0100

    More of yesterday's changes

--- src/utilities.c
+++ src/utilities.c
@@ -3,6 +3,20 @@
 #include <Rinternals.h>
 #include <Rmath.h>
 
+/* =============================================================================
+ * This function checks to see if a resource is of the correct type combination
+ * ========================================================================== */
+int get_rand_int(int from, int to){
+    
+    int rand_value;
+    
+    do{
+        rand_value = (int) floor( runif(from, to) );
+    }while(rand_value == to  );
+    
+    return rand_value;
+}
+
 
 /* =============================================================================
  * This function checks to see if a resource is of the correct type combination
