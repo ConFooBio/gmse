98319ffe24785d61ad1eba3d21d5a6b914c4de68
Brad Duthie <brad.duthie@gmail.com>
2017-06-16 11:40:47 +0100

    Fix utility layer testing

--- tests/testthat/test-utility_layer.R
+++ tests/testthat/test-utility_layer.R
@@ -8,7 +8,7 @@ res_types    <- rbind(c(1, 0, 0), c(2, 0, 0));
 layer <- utility_layer(agent_IDs, agent_number, res_types);
 
 test_that("Action and cost layers initialised with correct dimensions", {
-    expect_equal(dim(layer), c(8, 13));
+    expect_equal(dim(layer), c(13, 13));
 })
 
 test_that("Action and cost layers initialised with zeros", {
@@ -16,11 +16,9 @@ test_that("Action and cost layers initialised with zeros", {
 })
 
 test_that("Action and cost layers initialised with correct agents", {
-    expect_equal(c(min(layer[,1]),max(layer[,1])), c(1,4));
+    expect_equal(c(min(layer[,1]),max(layer[,1])), c(-2,4));
 })
 
 test_that("Action and cost layers initialised with correct resources", {
     expect_equal(c(min(layer[,2]),max(layer[,2])), c(1,2));
 })
-
-set.seed(Sys.time())
