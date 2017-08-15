bcb04902e00220b14df79469c5ace49572a2e6bd
Brad Duthie <brad.duthie@gmail.com>
2017-08-15 16:48:18 +0100

    Figured out how to get the inputs working in the body

--- notebook/gui_notes.R
+++ notebook/gui_notes.R
@@ -54,7 +54,7 @@ sidebar <-   dashboardSidebar(
                 
                 hr(style ="width: 70%; color: white; align: center"),
                 
-                menuItem(" Plotting", icon = icon("line-chart"), tabName = "Config"),
+                menuItem("Plotting", icon = icon("line-chart"), tabName = "plotting"),
 
                 menuItem("Source code for app", icon = icon("code"),
                          href = "https://github.com/bradduthie/gmse"
@@ -64,109 +64,56 @@ sidebar <-   dashboardSidebar(
 )
 
 
-# This makes the main body of the gui -- where the plot will probably go for GMSE
+
 body <- dashboardBody(
-    
-    includeCSS(system.file("www/elementR.css", package="elementR")),
-    
-    div(style = "min-height:100vh; min-width: (100vw - 230); display:flex",
-        div(style = "background-color: #726a5e; width: 31px;",
-            div(style = "background-color: #726a5e; width: 30px;position:fixed",
-                div(style = "background: rgb(0, 100, 0); height: 50px"),
-                uiOutput('ValidFlag1'),
-                div(style = "background-color: #726a5e; height: 30px; width: 30px"),
-                uiOutput('ValidFlag2'),
-                div(style = "background-color: #726a5e; height: 30px; width: 30px"),
-                uiOutput('ValidFlag3'),
-                div(style = "background-color: #726a5e; height: 30px; width: 30px"),
-                uiOutput('ValidFlag4'),
-                div(style = "background-color: #726a5e; height: 30px; width: 30px"),
-                uiOutput('ValidFlag5')
-            )
+    tabItems(
+        tabItem("global",
+
+                sliderInput("time",
+                            "Number of generations",
+                            min = 10,
+                            max = 200,
+                            step  = 10,
+                            value = 100)
+                
         ),
-        div(style = "width: 100%; margin-top:10px; margin-left:10px;margin-bottom:10px;margin-right:0px;",
-            uiOutput("TopBar"),
-            tabItems(
+        tabItem("resource",
                 
-                tabItem("start",
-                        uiOutput("start1"),
-                        
-                        fluidRow(
-                            uiOutput("start2")
-                        )
-                        
-                        
-                        
-                ), #eo tab start
+                selectInput("variable", "Model type:",
+                            c("Numerical Model"        = "Numer", 
+                              "Individual-Based Model" = "IBM")),
                 
+                sliderInput("time",
+                            "Number of generations",
+                            min = 10,
+                            max = 200,
+                            step  = 10,
+                            value = 100),
                 
-                tabItem("Standards", style = "padding-right: 0px; padding-left: 0px",
-                        uiOutput("Standards1"),
-                        uiOutput("Standards2")
-                        
-                        
-                        
-                ), #eo tab Standards
-                tabItem("MachDrift", style = "padding-right: 0px; padding-left: 0px",
-                        uiOutput("MachDrift1"),
-                        uiOutput("MachDrift2"),
-                        uiOutput('MachDrift3')
-                        
-                ), #eo tab MachDrift
+                sliderInput("N_initial",
+                            "Initial prey density",
+                            min   = 0,
+                            max   = 1,
+                            step  = 0.01,
+                            value = 0.5)
+        ),
+        tabItem("observation",
+                "Sub-item 1 tab content"
+        ),
+        tabItem("manager",
+                "Sub-item 2 tab content"
+        ),
+        tabItem("user",
+                "Sub-item 2 tab content"
+        ),
+        tabItem("run",
+                actionButton("go", "Run"),
                 
-                tabItem("Samples", style = "padding-right: 0px; padding-left: 0px",
-                        box(width = 12,background = "aqua", style = "background-color: #726a5e;margin-bottom:10px",
-                            column(5,
-                                   uiOutput("sample1")
-                            ),
-                            column(2,
-                                   uiOutput("sample2")
-                            ),
-                            column(3,
-                                   uiOutput("sample3")
-                            ),
-                            column(1, class = "class2",
-                                   br(),
-                                   uiOutput("sample4")
-                            )
-                            
-                        ),
-                        column(12,
-                               uiOutput("Sample5"))
-                        
-                ), #eo tab Samples
-                
-                tabItem("realign",
-                        uiOutput("realign1"),
-                        uiOutput("realign2"),
-                        uiOutput("realign8"),
-                        uiOutput("realign10"),
-                        fluidRow(
-                            column(3, uiOutput("realign3")),
-                            column(9, uiOutput("realign5"))
-                        )
-                        
-                        
-                        
-                        
-                ), #eo tab realign
-                tabItem("Config",
-                        uiOutput("config0"),
-                        uiOutput("config4"),
-                        uiOutput("config2"),
-                        uiOutput("config3"),
-                        uiOutput("config1")
-                        
-                ), #eo tab Config
-                tabItem("SessionConfig",
-                        uiOutput("Precision1"),
-                        uiOutput("Precision4")
-                ) #eo tab Config
-                
-            )
+                actionButton("reset", "Reset")
         )
     )
-)#dashboardBody
+)
+
 
 
 # This function builds the fancy title at the top of the browser
@@ -192,42 +139,14 @@ server <- function(input){
                         height = 85,
                         width = 12,
                         column(9,
-                               div(h3(icon("cogs"),"Step 1. Create a new project or load an existing one"), style = "display: inline-block;")
+                               div(h3(icon("cogs"),"Set the global parameters here"), style = "display: inline-block;")
                         )
                     )
                 )
             })
             
-            output$start2 <- renderUI({
-                
-                div(
-                    box(
-                        title = list(icon("folder-o"),"New Project"),
-                        width = 6,
-                        status="primary",
-                        solidHeader = TRUE,
-                        p("1. Choose the project folder"),
-                        actionButton("createProjButton", "Create your project !"),
-                        actionButton("runExampleNew", "Run Example")
-                    ),
-                    box(
-                        title = list(icon("folder"),"Load Project"),
-                        width = 6,
-                        solidHeader = TRUE,
-                        status="primary",
-                        p("1. Choose a project to load"),
-                        actionButton("loadProjButton","Load your Project"),
-                        actionButton("runExampleLoad", "Load Example")
-                        
-                    )
-                    
-                )
-                
-                
-                
-            })
         }
-    
+    })
     
 }    
 
@@ -238,8 +157,6 @@ server <- function(input){
 ######################
 app <- shinyApp(ui, server)
     runApp(app, launch.browser = TRUE)
-
-
 } #
 
 
