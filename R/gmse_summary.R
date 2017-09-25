dd3ebdd9fe4dc5aa7705ac695174ce45e9150595
Brad Duthie <brad.duthie@gmail.com>
2017-09-25 14:43:54 +0100

    Add the row trick to make flexible for future multiple resources

--- R/gmse_summary.R
+++ R/gmse_summary.R
@@ -55,13 +55,14 @@ gmse_summary <- function(gmse_results){
     colnames(observations) <- res_colna;
     #--- Next get the costs set by the manager
     costs <- matrix(dat = NA, nrow = time_steps * length(res_types), ncol = 8);
+    row   <- 1;
     for(i in 1:time_steps){
         manager_acts <- gmse_results$action[[i]][,,1];
         for(j in 1:length(res_types)){
             costs[i, 1]  <- i;
             costs[i, 2]  <- res_types[j];
-            estim_row  <- which(manager_acts[,1] ==  1 & 
-                                    manager_acts[,2] == res_types[j]);
+            estim_row    <- which(manager_acts[,1] ==  1 & 
+                                  manager_acts[,2] == res_types[j]);
             if(parameters[89] == TRUE){
                 costs[i, 3] <- manager_acts[estim_row,  8];
             }
@@ -77,15 +78,26 @@ gmse_summary <- function(gmse_results){
             if(parameters[93] == TRUE){
                 costs[i, 7] <- manager_acts[estim_row,  12];
             }
-            costs[i, 8] <- manager_acts[estim_row, 13] - parameters[97];
+            costs[row, 8] <- manager_acts[estim_row, 13] - parameters[97];
+            row <- row + 1;
         }
     }
+        
     cost_col <- c("time_step", "resource_type", "scaring", "culling",
                   "castration", "feeding", "helping", "unused");
     colnames(costs) <- cost_col;
     #--- Next get the actions of users
-    
-    
+    users   <- sum(gmse_results$agents[[1]][,2] > 0);
+    actions <- matrix(dat  = NA, nrow = time_steps * length(res_types) * users,
+                      ncol = 13);
+    for(i in 1:time_steps){
+        for(j in 2:users){
+            for(k in 1:length(res_types)){
+                
+                
+            }
+        }
+    }
     
 }
 
