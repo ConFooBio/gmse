513d4d25cf66f6b33adea42a643f7e968d40b7e6
Brad Duthie <brad.duthie@gmail.com>
2017-10-11 14:27:21 +0100

    gc

--- R/gmse_apply.R
+++ R/gmse_apply.R
@@ -71,26 +71,24 @@ gmse_apply <- function(res_mod  = resource,
     arg_vals    <- update_para_vec(arg_list   = arg_vals);
     
     # ------ USER MODEL --------------------------------------------------------
-    #usr_args    <- prep_usr(arg_list = arg_vals, usr_mod = use_mod);
-    #check_args(arg_list = usr_args, the_fun = use_mod);
-    #usr_results <- do.call(what = use_mod, args = usr_args);
-    #usr_results <- check_name_results(output   = usr_results, 
-    #                                  vec_name = "user_vector", 
-    #                                  mat_name = "user_array");
-    #arg_vals    <- add_results(arg_list = arg_vals, output = usr_results);
-    #arg_vals    <- fix_gmse_defaults(arg_list = arg_vals, model = use_mod);
-    #arg_vals    <- translate_results(arg_list = arg_vals, output = usr_results);
-    #arg_vals    <- update_para_vec(arg_list   = arg_vals);
+    usr_args    <- prep_usr(arg_list = arg_vals, usr_mod = use_mod);
+    check_args(arg_list = usr_args, the_fun = use_mod);
+    usr_results <- do.call(what = use_mod, args = usr_args);
+    usr_results <- check_name_results(output   = usr_results, 
+                                      vec_name = "user_vector", 
+                                      mat_name = "user_array");
+    arg_vals    <- add_results(arg_list = arg_vals, output = usr_results);
+    arg_vals    <- fix_gmse_defaults(arg_list = arg_vals, model = use_mod);
+    arg_vals    <- translate_results(arg_list = arg_vals, output = usr_results);
+    arg_vals    <- update_para_vec(arg_list   = arg_vals);
     
     return(arg_vals);    
 }
 
 ################################################################################
+# Subfunctions required                                                        #
 ################################################################################
-################################################################################
-################################################################################
-################################################################################
-################################################################################
+
 
 fun_warn <- function(res_mod, obs_mod, man_mod, use_mod){
     if( is.function(res_mod) == "FALSE" ){
@@ -406,6 +404,7 @@ fix_gmse_defaults <- function(arg_list, model){
     if( identical(model, manager) == TRUE ){
         arg_list <- copy_args(arg_list, "RESOURCES", "resource_array");
         arg_list <- copy_args(arg_list, "COST", "manager_array");
+        arg_list <- copy_args(arg_list, "ACTION", "user_array");
     }
     if( identical(model, user) == TRUE ){
         arg_list <- copy_args(arg_list, "RESOURCES", "resource_array");
@@ -511,10 +510,15 @@ translate_results <- function(arg_list, output){
             rows    <- which(arg_list$user_array[,1,1] == -2);
             arg_list$user_array[rows, 9, ] <- divuser;
         }
-        if(out_names[[i]] == "user_array" | out_names[[i]] == "ACTIONS"){
+        if(out_names[[i]] == "user_array" | out_names[[i]] == "ACTION"){
+            
+            
+            
+            
+            
             u_out <- arg_list$user_array;
-            t_use <- length(u_out);
-            acts  <- u_out[u_out[,1,1] == -2, 9, ];
+            rows  <- which(u_out[,1,1] == -2);
+            acts  <- u_out[rows, 9, ];
             if(is.matrix(acts) == TRUE){
                 allac <- apply(X = acts, MARGIN = 1, FUN = sum);  
             }else{
@@ -1013,195 +1017,7 @@ add_usr_defaults <- function(arg_list){
     return(arg_list);
 }
 
-
-
-
 ################################################################################
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-
-
-
-
-pass_paras <- function( time_max = 100, land_dim_1 = 100, land_dim_2 = 100,  
-                        res_movement = 20, remove_pr = 0.0, lambda = 0.30, 
-                        agent_view = 10, agent_move = 50, res_birth_K = 10000,
-                        res_death_K = 2000, edge_effect = 1, res_move_type = 1,
-                        res_birth_type = 2, res_death_type = 2, 
-                        observe_type = 0, fixed_mark = 50, fixed_recapt = 150, 
-                        times_observe = 1, obs_move_type  = 1, res_min_age = 0,
-                        res_move_obs = TRUE, Euclidean_dist = FALSE, 
-                        plotting = FALSE, hunt = FALSE, start_hunting = 95,  
-                        res_consume = 0.5, ga_popsize = 100, ga_mingen = 40, 
-                        ga_seedrep = 20, ga_sampleK = 20, ga_chooseK = 2,  
-                        ga_mutation = 0.1, ga_crossover = 0.1, 
-                        move_agents = TRUE, max_ages = 5, minimum_cost = 10,
-                        user_budget = 1000, manager_budget = 1000,
-                        manage_target = 1000, RESOURCE_ini = 1000, 
-                        scaring = FALSE, culling = TRUE, castration = FALSE,
-                        feeding = FALSE, help_offspring = FALSE, 
-                        tend_crops = FALSE, tend_crop_yld = 0.2, 
-                        kill_crops = FALSE, stakeholders = 4, 
-                        manage_caution = 1, land_ownership = FALSE, 
-                        manage_freq = 1, converge_crit = 100, 
-                        manager_sense = 0.1, public_land    = 0, ...
-                    ){
-    
-    input_list <- c(time_max, land_dim_1, land_dim_2, res_movement, remove_pr,
-                    lambda, agent_view, agent_move, res_birth_K, res_death_K,
-                    edge_effect, res_move_type, res_birth_type, res_death_type,
-                    observe_type, fixed_mark, fixed_recapt, times_observe,
-                    obs_move_type, res_min_age, res_move_obs, Euclidean_dist, 
-                    plotting, hunt, start_hunting, res_consume, ga_popsize,
-                    ga_mingen, ga_seedrep, ga_sampleK, ga_chooseK, ga_mutation,
-                    ga_crossover, move_agents, max_ages, minimum_cost,
-                    user_budget, manage_target, RESOURCE_ini, scaring, culling,
-                    castration, feeding, help_offspring, tend_crops,
-                    tend_crop_yld, kill_crops, stakeholders, manage_caution,
-                    land_ownership, manage_freq, converge_crit, manager_sense,
-                    public_land); 
-    
-    user_res_opts  <- c(scaring, culling, castration, feeding, help_offspring);
-    user_lnd_opts  <- c(tend_crops, kill_crops);
-    
-    ttr <- 20;
-    agn <- stakeholders + 1;
-    agt <- 17;
-    lkr <- 2;
-    lyr <- stakeholders + 1;
-    roc <- stakeholders + 1;
-    coc <- 13;
-
-    paras <- c(1, edge_effect, res_move_type, res_birth_type, res_death_type,
-               res_birth_K, res_death_K, 0, observe_type, 1, fixed_mark, 
-               times_observe, land_dim_1, land_dim_2, obs_move_type, 1, 
-               res_min_age, 1, 12, res_move_obs, Euclidean_dist, ga_popsize, 
-               ga_mingen, ga_seedrep, ga_sampleK, ga_chooseK, ga_mutation,
-               ga_crossover, move_agents, max_ages, 7, 11, RESOURCE_ini, 4, 5,
-               6, 3, 9, 10, 18, 19, ttr, 16, 8, 1, 1, 15, 14, 1, 4, 5, 6, 10, 
-               12, agn, agt, 1, 2, 3, 13, lkr, RESOURCE_ini, ttr+times_observe, 
-               1, 0, lyr, lkr-1, 8, roc, coc, 4, 7, 0, 17, 0, 
-               -1*manager_sense*(1+lambda), -1*manager_sense*lambda, 1*lambda, 
-               1*manager_sense, tend_crop_yld, 1, 2, 15, 0, 0, 0, 0, 0, 
-               user_res_opts[1], user_res_opts[2], user_res_opts[3], 
-               user_res_opts[4], user_res_opts[5], user_lnd_opts[1], 
-               user_lnd_opts[2], manage_caution, minimum_cost, user_budget, 
-               converge_crit, RESOURCE_ini, 0, 0, fixed_recapt, land_ownership, 
-               public_land, lambda
-    );
-    
-    return( list(gmse_user_input = as.vector(input_list), 
-                 gmse_para_vect  = as.vector(paras))
-          );
-}
-
-
-
-get_arg_list <- function(the_function, all_arg_names, all_arg_values){
-    fun_args <- names(formals(the_function));
-    fun_vals <- rep(x = NA, times = length(fun_args)); 
-    for(i in 1:length(fun_args)){
-        for(j in 1:length(all_arg_names)){
-            if( fun_args[i] == "RESOURCES" &         # Handles an exception
-                all_arg_names[j] == "resource_arr"){
-                fun_vals[i] <- all_arg_values[j];
-            }
-            if( fun_args[i] == "resource_arr" &         # Handles an exception
-                all_arg_names[j] == "RESOURCES"){
-                fun_vals[i] <- all_arg_values[j];
-            }
-            if( identical(fun_args[i], all_arg_names[j]) == TRUE){
-                fun_vals[i] <- all_arg_values[j];
-            }
-        }
-    }
-    for(i in 1:length(fun_args)){
-        if(is.na(fun_vals)[i] == TRUE & formals(the_function)[i] != ""){
-            fun_vals[i] <- formals(the_function)[i];
-        }
-    }
-    return(fun_vals);
-}
-
-
-
-
-
-update_all_arguments <- function(mod_output, all_arguments, all_arg_names){
-    for(i in 1:length(mod_output)){
-        for(j in 1:length(all_arguments)){
-            if(names(mod_output)[i] == all_arg_names[j]){
-                all_arguments[j] <- mod_output[i];
-            }
-        }
-    }
-    return(all_arguments);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#=== SCRATCH BELOW
-
-
-f1 <- function(x, z = 0, m = NULL) 2*x + z;
-f2 <- function(x) x*x + 1;
-
-dv <- function(d1 = -1, d2 = -2, ...){
-    return(c(d1, d2));
-}
-
-xfun <- function(f1_i, f2_i, x, y, ...){
-    
-    f1 <- match.fun(f1_i);
-    f2 <- match.fun(f2_i);
-    
-    #lists all of the arguments in the function
-    llv      <- as.list(sys.call()); # All values
-    lln      <- names(llv);          # All names
-    f1_args  <- names(formals(f1));  # Just the names of F1 arguments
-    
-    #Isolates the ... arguments relevant to f1
-    f1_input <- rep(x = 0, times = length(f1_args));
-    for(i in 1:length(lln)){
-        for(j in 1:length(f1_args)){
-            if(lln[i] == f1_args[j]){
-                f1_input[j] <- llv[i];
-            }
-        }
-    }
-
-    # Runs f1 with the relevant arguments
-    res <- do.call(what = f1, args = f1_input);
-    
-    
-    
-    f1x <- match.fun(f1);
-    f2x <- match.fun(f2);
-    
-    f1r <- f1x(x);
-    f2r <- f2x(y);
-    
-    v1  <- c(f1r, f2r)
-    v2  <- dv(...);
-    
-    return( x );
-    
-}
-
 
 
 
@@ -1268,127 +1084,14 @@ HarvDec1 <- function(HD_type="A", c=1000, qu=0.2, observation_vec=100){
     TAC
 }
 
-#observation(RESOURCES = sim$resource[[1]], LAND = sim$land[[1]], PARAS = sim$paras[1,], AGENTS = sim$agents[[1]], inter_tabl = tbb, fix_mark = 50, times = 1, samp_age = 0, agent_type = 0, type_cat = 1, obs_method = 0, move_res = TRUE, model  = "IBM")
-
-
-#test <- NULL;
-#testrows <- NULL;
-#for(i in 1:99){
-#    test[[i]] <- gmse_apply(resource_model = resource, RESOURCES = sim$resource[[i]]);
-#    testrows  <- rbind(testrows, c(i, dim(test[[i]][[2]][[1]])[1], test[[i]][[2]][[3]][33]));
-#}
-
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-
-
-argument_list <- function(res_mod, obs_mod, man_mod, use_mod, oth_vals){
-    oth_names   <- names(oth_vals);
-    res_names   <- names(formals(res_mod));
-    obs_names   <- names(formals(obs_mod));
-    man_names   <- names(formals(man_mod));
-    use_names   <- names(formals(use_mod));  
-    f_arg_names <- c(res_names, obs_names, man_names, use_names);
-    r_arg_names <- c("resource_array", "resource_vector",
-                     "observation_array", "observation_vector",
-                     "manager_array", "manager_vector",
-                     "user_array", "user_vector");
-    t_arg_names <- c(f_arg_names, r_arg_names, oth_names);
-    u_arg_names <- unique(t_arg_names);
-    u_arg_names <- u_arg_names[u_arg_names != ""];
-    all_names   <- u_arg_names[u_arg_names != "..."];
-    arg_list    <- rep(x = list(NA), times = length(all_names));
-    arg_list    <- place_args(all_names, oth_vals, arg_list);
-    arg_list    <- place_args(all_names, formals(res_mod), arg_list);
-    arg_list    <- place_args(all_names, formals(obs_mod), arg_list);
-    arg_list    <- place_args(all_names, formals(man_mod), arg_list);
-    arg_list    <- place_args(all_names, formals(use_mod), arg_list);
-    arg_out     <- list(all_arg_values = arg_list, all_arg_names = all_names);
+Impl1 <- function(manager_vector = 0, ModType="A", p=0.7){
+    H_I <- switch(ModType,
+                  A={rbinom(1, size=manager_vector, p=p)},
+                  B=(rpois(n=1, lambda=manager_vector*p)))
     
-    return(arg_out);        
-}
-
-
-place_args <- function(all_names, placing_vals, arg_list){
-    placing_names <- names(placing_vals);
-    empty         <- identical(placing_names, NULL);
-    if(empty == TRUE){
-        return(arg_list);
-    }
-    for(i in 1:length(placing_vals)){
-        place_name <- placing_names[i];
-        if(place_name %in% all_names){
-            place_pos <- which(all_names == place_name);
-            arg_eval  <- eval(placing_vals[[i]]);
-            if(is.null(arg_eval) == FALSE){
-                arg_list[[place_pos]] <- eval(placing_vals[[i]]);
-            }
-        }
-    }
-    return(arg_list);
-}
-
-
-
-
-replace_default_args <- function(argument_list, argument_names, from, to){
-    from_names <- names(from);
-    if("RESOURCES" %in% from_names){
-        RESOURCE_pos <- which(argument_names == "RESOURCES");
-        res_arr_pos  <- which(argument_names == "resource_array");
-        res_vec_pos  <- which(argument_names == "resource_vector");
-        argument_list[[res_arr_pos]] <- argument_list[[RESOURCE_pos]];
-    }
+    H_I
     
-    
-    
-    return( list(new_args = new_args) );
 }
-
-
-fun_warn <- function(res_mod, obs_mod, man_mod, use_mod){
-  if( is.function(res_mod) == "FALSE" ){
-    stop( "ERROR: Resource model needs to be a function");
-  }
-  if( is.function(obs_mod) == "FALSE" ){
-    stop( "ERROR: Observation model needs to be a function");
-  }
-  if( is.function(man_mod) == "FALSE" ){
-    stop( "ERROR: Manager model needs to be a function");
-  }
-  if( is.function(use_mod) == "FALSE" ){
-    stop( "ERROR: User model needs to be a function");
-  }
-}
-
-
-swap_vec_to_mat <- function(vec_name, mat_name, arg_list, arg_names, model){
-  
-  
-  if(model == "resource"){
-    
-  }
-  
-  return(argument_list);
-}
-
-swap_mat_to_vec <- function(mat_name, vec_name, arg_list, arg_names, model){
-  
-  return(argument_list)
-}
-
-
-
-
-
-
-
-
-
 
 
 
