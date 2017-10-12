2ed989f8d49085e6cee8509fa465b6f3a9b8ace1
Brad Duthie <brad.duthie@gmail.com>
2017-10-12 12:05:38 +0100

    Update variable names

--- R/observation.R
+++ R/observation.R
@@ -8,13 +8,13 @@
 #'@param PARAS The vector of parameters that hold global and dynamic parameter values used by GMSE
 #'@param AGENTS The array of agents produced in the main gmse() function
 #'@param inter_tabl Interaction table indexing types with the INTERACT matrix
-#'@param fix_mark Fixed number of individuals marked? (A number, or FALSE)
-#'@param times Number of times that the observations are made (e.g., managers go out sampling n times in an area of the landscape)
-#'@param samp_age Minimum age of the resource being sampled (default = 1)
+#'@param fixed_mark Fixed number of individuals marked? (A number, or FALSE)
+#'@param times_observe Number of times that the observations are made (e.g., managers go out sampling n times in an area of the landscape)
+#'@param res_min_age Minimum age of the resource being sampled (default = 1)
 #'@param agent_type The type of agent doing the observing (default = 0)
 #'@param type_cat The category of agent type (first 4 columns) doing observing; this will almost always be 1, so type 0 agents (managers, of which there is always one by default) will perform the observations
-#'@param observe_type The type of method used to do the observing. For types of observation exist: (1) Density based observation, where observers count all of the resources within a subset of the landscape (the manager function can then later estimate total resource number from this estimate). (2) Mark-recapture based observation, where observers tag a fixed number of randomly sampled resources on the landscape some number of `times`; some of these resources marks are later interpreted as marks ('fix_mark') while the rest are interpreted as recaptures. (3) Transect based observation, where observers sample a linear transect, observing all resources on the transect one row of landscape cells at a time, until all landscape cells are sampled; between samples, resources might move generating observation error. (4) Block based sampling, which is very similar to Transect based sampling; here observers instead sample square blocks of a landscape, counting resources one block at a time, until the whole landscape is sampled; between samples resources might move generating observation error.
-#'@param move_res Defines whether or not resources move during observation (default = FALSE). Note that if this is FALSE, then observation methods (observe_type) 3 and 4 produce no observation error 
+#'@param observe_type The type of method used to do the observing. For types of observation exist: (1) Density based observation, where observers count all of the resources within a subset of the landscape (the manager function can then later estimate total resource number from this estimate). (2) Mark-recapture based observation, where observers tag a fixed number of randomly sampled resources on the landscape some number of `times`; some of these resources marks are later interpreted as marks ('fixed_mark') while the rest are interpreted as recaptures. (3) Transect based observation, where observers sample a linear transect, observing all resources on the transect one row of landscape cells at a time, until all landscape cells are sampled; between samples, resources might move generating observation error. (4) Block based sampling, which is very similar to Transect based sampling; here observers instead sample square blocks of a landscape, counting resources one block at a time, until the whole landscape is sampled; between samples resources might move generating observation error.
+#'@param res_move_obs Defines whether or not resources move during observation (default = FALSE). Note that if this is FALSE, then observation methods (observe_type) 3 and 4 produce no observation error 
 #'@param model The type of model being applied (Currently only individual-based
 #' -- i.e., 'agent-based' -- models are allowed)
 #'@param ... Other arguments to be passed to a user-defined model
@@ -22,9 +22,9 @@
 #'@examples
 #'\dontrun{
 #'OBSERVATION_NEW <- observation(RESOURCES = RESOURCES, LAND = LANDSCAPE_r,
-#'PARAS = paras, AGENTS = AGENTS, inter_tabl = interaction_tabl, fix_mark = fxo,
-#'times = tmo, samp_age = rma, agent_type = 0, type_cat   = 1, observe_type = obt,
-#'move_res   = rmo);
+#'PARAS = paras, AGENTS = AGENTS, inter_tabl = interaction_tabl, fixed_mark = fxo,
+#'times_observe = tmo, res_min_age = rma, agent_type = 0, type_cat   = 1, observe_type = obt,
+#'res_move_obs   = rmo);
 #'}
 #'@export
 observation <- function(RESOURCES     = NULL, 
@@ -59,12 +59,12 @@ observation <- function(RESOURCES     = NULL,
         # If all checks out, first put the type into PARAS for easier input
         PARAS[8]  <- agent_type;
         PARAS[9]  <- observe_type;
-        PARAS[11] <- as.numeric(fix_mark); # Note: 'FALSE' coerced to zero
-        PARAS[12] <- times;
-        PARAS[17] <- samp_age;
+        PARAS[11] <- as.numeric(fixed_mark); # Note: 'FALSE' coerced to zero
+        PARAS[12] <- times_observe;
+        PARAS[17] <- res_min_age;
         PARAS[18] <- type_cat;
-        PARAS[20] <- as.numeric(move_res);
-        if(times < 1){
+        PARAS[20] <- as.numeric(res_move_obs);
+        if(times_observe < 1){
             stop("Need to sample at least once (else no observation)");   
         }
         # Then run the population model
