8c7464de79354d55d13f7f6f6131246bb0c4bbd9
Brad Duthie <brad.duthie@gmail.com>
2018-04-30 16:00:36 +0100

    Major add of vignettes and relevant data

---
+++ R/SI4_data.R
@@ -0,0 +1,44 @@
+#' @name sim_init_basic
+#' @title R data needed for quick load basic sim in the S4 vignette
+#' @description This object contains SI4 vignette simulation output
+#' @docType data
+#' @usage sim_init_basic
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
+
+#' @name sim_old_1_LAND
+#' @title R data needed to show vignette example of LAND 1
+#' @description This object contains simulation output for SI4 for LAND 1
+#' @docType data
+#' @usage sim_old_1_LAND
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
+
+#' @name sim_old_2_LAND
+#' @title R data needed to show vignette example of LAND 2
+#' @description This object contains simulation output for SI4 for LAND 1
+#' @docType data
+#' @usage sim_old_2_LAND
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
+
+#' @name sim_sum_1
+#' @title R data needed to show a summary of example simulation 1
+#' @description This object contains simulation the simulation 1 example in S4
+#' @docType data
+#' @usage sim_sum_1
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
+
+#' @name sim_sum_2
+#' @title R data needed to show a summary of example simulation 2
+#' @description his object contains simulation the simulation 2 example in S4
+#' @docType data
+#' @usage sim_sum_2
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
