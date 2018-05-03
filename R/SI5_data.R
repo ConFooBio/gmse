5f44e6fa5b49179e9b629235632958c9f51a9758
Brad Duthie <brad.duthie@gmail.com>
2018-05-03 12:26:14 +0100

    Update data and default R values in documentation

---
+++ R/SI5_data.R
@@ -0,0 +1,17 @@
+#' @name rec.n
+#' @title R data for recruitment used in SI4 vignette
+#' @description This object contains SI4 vignette simulation output
+#' @docType data
+#' @usage rec.n
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
+
+#' @name ssb.n
+#' @title R data for spawning stock biomass used in SI4 vignette
+#' @description This object contains SI4 vignette simulation output
+#' @docType data
+#' @usage ssb.n
+#' @format a set of \code{gmse_apply} output examples
+#' @author Brad Duthie
+NULL
