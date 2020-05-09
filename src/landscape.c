4be098097aebd1818f83903f305032098222d672
Brad Duthie <brad.duthie@gmail.com>
2020-05-09 15:27:50 +0100

    Landscape breakdown not working yet, but recursion works to spread ownership across the landscape unevenly

--- src/landscape.c
+++ src/landscape.c
@@ -1,11 +1,49 @@
 #include "utilities.h"
 
-void break_land(int **land, int x0, int x1, int y0, int y1, int N, int *count){
+
+
+void break_land(int **land, int x0, int x1, int y0, int y1, int N, int *count,
+                int *bin){
     
-    int xx, yy;
+    int xx, yy, halfway, N1, N2, x0_new;
     
     if(N > 1){
-        
+        if( (x1 - x0) > (y1 - y0) ){
+            halfway  = floor( 0.5 * (x0 + x1) );
+            if( (halfway % 2) != 0){
+                halfway = floor(halfway) + (*bin);
+                (*bin)   = (1 - *bin) * (1 - *bin); /* Swap between 0 and 1 */
+            }
+            N1       = floor(0.5 * N);
+            N2       = N1 + (N % 2);
+            break_land(land, x0, halfway, y0, y1, N1, count, bin);
+            break_land(land, halfway, x1, y0, y1, N2, count, bin);
+        }
+        if( (x1 - x0) < (y1 - y0) ){
+            halfway  = floor( 0.5 * (y0 + y1) );
+            if( (halfway % 2) != 0){
+                halfway = floor(halfway) + (*bin);
+                (*bin)   = (1 - *bin) * (1 - *bin); /* Swap between 0 and 1 */
+            }
+            N1       = floor(0.5 * N);
+            N2       = N1 + (N % 2);
+            break_land(land, x0, x1, y0, halfway, N1, count, bin);
+            break_land(land, x0, x1, halfway, y1, N2, count, bin);
+        }
+        if( (x1 - x0) == (y1 - y0) && (*bin) == 0){
+            halfway  = floor( 0.5 * (y0 + y1) );
+            N1       = floor(0.5 * N);
+            N2       = N1;
+            break_land(land, x0, x1, y0, halfway, N1, count, bin);
+            break_land(land, x0, x1, halfway, y1, N2, count, bin);
+        }
+        if( (x1 - x0) == (y1 - y0) && (*bin) == 1){
+            halfway  = floor( 0.5 * (x0 + x1) );
+            N1       = floor(0.5 * N);
+            N2       = N1;
+            break_land(land, x0, x1, y0, halfway, N1, count, bin);
+            break_land(land, x0, x1, halfway, y1, N2, count, bin);
+        }
     }else{
         for(xx = x0; xx < x1; xx++){
             for(yy = y0; yy < y1; yy++){
@@ -29,7 +67,7 @@ SEXP build_ownership(SEXP PARAMETERS){
     /* SOME STANDARD DECLARATIONS OF KEY VARIABLES AND POINTERS               */
     /* ====================================================================== */
     int xloc, yloc, dim_x, dim_y, vec_pos, owners, p_land;
-    int **land, *count;
+    int **land, *count, *bin;
     int protected_n, len_PARAMETERS;
     double *paras_ptr, *land_ptr_new, *build_paras;
     
@@ -59,7 +97,7 @@ SEXP build_ownership(SEXP PARAMETERS){
     
     /* Do the biology here now */
     /* ====================================================================== */
-    
+    bin    = malloc(1 * sizeof(int));
     count  = malloc(1 * sizeof(int));
     land   = malloc(dim_x * sizeof(int *));
     for(xloc = 0; xloc < dim_x; xloc++){
@@ -72,10 +110,10 @@ SEXP build_ownership(SEXP PARAMETERS){
         }
     }
     
-    *count = 3;
+    (*bin)   = 0;
+    (*count) = 1;
     
-    break_land(land, 0, 1, 0, 2, 1, count);
-    break_land(land, 1, 2, 0, 2, 1, count);
+    break_land(land, 0, dim_x, 0, dim_y, owners, count, bin);
 
     /* This code switches from C back to R */
     /* ====================================================================== */        
