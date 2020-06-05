d3d24fd11898c05cc328d2e731a898d49e46a28f
AdrianBach <43581266+AdrianBach@users.noreply.github.com>
2020-06-05 16:38:51 +0200

    forgot to add it in input list

--- R/gmse.R
+++ R/gmse.R
@@ -495,7 +495,7 @@ gmse <- function( time_max       = 40,    # Max number of time steps in sim
                     manager_sense, public_land, group_think, age_repr,
                     usr_budget_rng, action_thres, budget_bonus, consume_surv,
                     consume_repr, times_feeding, ownership_var, usr_yld_budget, 
-                    man_yld_budget); 
+                    man_yld_budget, mem_prv_observ); 
    
     paras_errors(input_list);
     
