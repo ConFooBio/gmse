72ac233e611b5f143739f6924db0c5d0bd6a8119
jejoenje <jeroenminderman@gmail.com>
2021-02-09 12:54:27 +0000

    Moved all notebook/gmse_apply_CONTROL.R code to R/gmse_apply_control.R and added documentation to all appropriate functions. Roxygenised, checked and rebuilt package and all functions now working as expected without further external loading. Also added appropriate extinction catch to gmse_apply_UROM() so that there is a check following the user model but before the resource model is run, to ensure function execution is stopped if the users wiped out the entire remaining population. Checked and working as expected.

--- notebook/gmse_apply_CONTROL_TESTS.R
+++ notebook/gmse_apply_CONTROL_TESTS.R
@@ -1,7 +1,14 @@
 rm(list=ls())
-source("notebook/gmse_apply_CONTROL.R")
-source("R/gmse_apply.R")
-rm(gmse_apply)
+
+call_bogus_for_debug = function(res_mod  = resource, 
+                                obs_mod  = observation, 
+                                man_mod  = manager, 
+                                use_mod  = user,
+                                get_res  = "basic",
+                                old_list = NULL,
+                                ...) {
+    return(sys.call())
+}
 
 LAND_OWNERSHIP = TRUE
 TEND_CROPS = TRUE
@@ -11,9 +18,9 @@ OBSERVE_TYPE = 1
 RES_MOVE_OBS = TRUE
 RES_DEATH_K = 3000
 LAMBDA = 0.3
-MANAGE_TARGET = 3000
+MANAGE_TARGET = 1500
 STAKEHOLDERS = 4
-USER_BUDGET = 1000
+USER_BUDGET = 1500
 MANAGER_BUDGET = 1000
 
 ### Initial time steps:
@@ -28,16 +35,23 @@ init_steps$observed_suggested
 
 
 ### User input
-costs_as_input = list(culling = 100001, scaring = 10)
+costs_as_input = list(culling = 10, scaring = 100001)
 prev = set_man_costs(prev, newcost = costs_as_input)
 
 ### Run next time step:
-nxt = gmse_apply_UROM(get_res = "Full", old_list = prev)
-# Add appropriate outputs.
-output = append_UROM_output(dat = nxt, costs = costs_as_input, old_output = output)
-# Reset time step
-prev = nxt
+nxt = try({gmse_apply_UROM(get_res = "Full", old_list = prev)}, silent = TRUE)
 
-### Output new results:
-output
-observed_suggested(prev)
+if(class(nxt)!="try-error") {
+    # Add appropriate outputs.
+    output = append_UROM_output(dat = nxt, costs = costs_as_input, old_output = output)
+    # Reset time step
+    prev = nxt
+    
+    ### Output new results:
+    observed_suggested(prev)
+    output
+} else {
+    print("STOP - POPULATION WIPED OUT")
+    output
+}
+    
