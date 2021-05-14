4e2edd7252eb739aeba3bc0b7b625125c3a3d347
Adrian Bach <adrian.bach@stir.ac.uk>
2021-05-14 11:58:00 +0200

    fix
    @bradduthie please use this one instead

--- compare.R
+++ compare.R
@@ -1,9 +1,10 @@
 library(GMSE)
 
-regular <- gmse_replicates(replicates = 10, time_max = 10, mem_prv_observ = FALSE, traj_pred = FALSE, plotting = FALSE, land_ownership = TRUE, stakeholders = 20)
-regular
+# regular <- gmse_replicates(replicates = 10, time_max = 10, mem_prv_observ = FALSE, traj_pred = FALSE, plotting = FALSE, land_ownership = TRUE, stakeholders = 20)
+# regular
 
-pred <- gmse_replicates(replicates = 10, time_max = 10, mem_prv_observ = TRUE, traj_pred = TRUE, plotting = FALSE, land_ownership = TRUE, stakeholders = 20)
+#pred <- gmse_replicates(replicates = 10, time_max = 10, mem_prv_observ = TRUE, traj_pred = TRUE, plotting = FALSE, land_ownership = TRUE, stakeholders = 20)
+pred <- gmse(time_max = 10, mem_prv_observ = TRUE, traj_pred = TRUE, plotting = FALSE, land_ownership = TRUE, stakeholders = 20)
 
 # Check abundance estimations
 pred$paras[,100]
