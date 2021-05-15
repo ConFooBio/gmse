9c41a1db4475d709475306504f48eb4d820a1091
Brad Duthie <brad.duthie@gmail.com>
2021-05-15 17:56:14 +0100

    Tweak the res_death_type option so that 0 now causes death to occur only due to users culling, and for no other reason. An additional res_death_type equals -1 causes no death for any reason at all. The 0 and 1 options can remain hidden in the documentation for now, because it is easy to go wrong with these (e.g., by allowing the population go grow out of controll, but @mattnuttall00 can now use this accurately. @mattnuttall00 -- you will need to now install the latest version from GitHub to run your simulations; I will send you a message on how

--- src/resource.c
+++ src/resource.c
@@ -208,7 +208,16 @@ void res_remove(double **res_removing, double *paras){
     csr             = paras[116]; /* Consumption required for survival */
     
     switch(type){
-        case 0: /* No removal */
+        case -1: /* No removal at all and no old age */
+            break;
+        case 0: /* No old age; only removal due to users */
+            for(resource = 0; resource < resource_number; resource++){
+                rm_from_Ind = res_removing[resource][rm_adj];
+                rand_unif   = runif(0, 1);
+                if(rand_unif < rm_from_Ind){
+                    res_removing[resource][rm_col] = -1;   
+                }
+            }
             break;
         case 1:
             for(resource = 0; resource < resource_number; resource++){
@@ -287,10 +296,10 @@ void res_remove(double **res_removing, double *paras){
             break;
     }
     for(resource = 0; resource < resource_number; resource++){
-        if(res_removing[resource][age_col] > max_age & type >= 0){
+        if(res_removing[resource][age_col] > max_age & type > 0){
             res_removing[resource][rm_col] = -1;
         }
-        if(res_removing[resource][cons_col] < csr & type >= 0){
+        if(res_removing[resource][cons_col] < csr & type > 0){
             res_removing[resource][rm_col] = -1;
         }
     }
