836e59c9aa5e2f8674f62785d865588f268860ff
Brad Duthie <brad.duthie@gmail.com>
2021-08-02 19:05:51 +0100

    Remove the gmse_apply test on CRAN

--- tests/testthat/test-make_gmse.R
+++ tests/testthat/test-make_gmse.R
@@ -3,19 +3,16 @@ library("GMSE");
 context("Main gmse function");
 
 test_that("Dimensions of simulation list output are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(length(sim), 8);
 })
 
 test_that("Dimensions of simulation resource output are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(length(sim[[1]]), 3);
 })
 
 test_that("Dimensions of simulation observation output are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(length(sim[[2]]), 3);
 })
@@ -27,14 +24,12 @@ test_that("Dimensions of simulation paras output are correct", {
 })
 
 test_that("Dimensions of simulation landscape output are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(length(sim[[4]]), 3);
     expect_equal(dim(sim[[4]][[1]]), c(100, 100, 3));
 })
 
 test_that("Dimensions of time elapsed are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(length(sim[[5]]), 5);
 })
@@ -46,20 +41,17 @@ test_that("Dimensions of simulation agent output are correct", {
 })
 
 test_that("Dimensions of simulation cost array output are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(dim(sim[[7]][[1]]), c(7, 13, 5));
 })
 
 test_that("Dimensions of simulation cost array output are correct", {
-    skip_on_cran();
     sim <- gmse(time_max = 3, plotting = FALSE);
     expect_equal(length(sim[[8]]), 3);
     expect_equal(dim(sim[[8]][[1]]), c(7, 13, 5));
 })
 
 test_that("Consumption based resource use is working", {
-    skip_on_cran();
     sim   <- gmse(time_max = 4, times_feeding = 4, consume_surv = 0.1, 
                   consume_repr = 0.4, res_consume = 0.2, land_dim_1 = 10,
                   land_dim_2 = 10, plotting = FALSE);
